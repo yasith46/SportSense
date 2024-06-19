27aef7a93daad73179b4b7ad8dec19db6973f345
JulianSilva2001 <sackjuliansilva@gmail.com>
2024-06-20 04:05:19 +0530

    Position detection for two joints

--- pose_landmarker/android/app/src/main/java/com/google/mediapipe/examples/poselandmarker/OverlayView.kt
+++ pose_landmarker/android/app/src/main/java/com/google/mediapipe/examples/poselandmarker/OverlayView.kt
@@ -30,6 +30,7 @@ import com.google.mediapipe.tasks.vision.poselandmarker.PoseLandmarkerResult
 import kotlin.math.max
 import kotlin.math.min
 import com.google.mediapipe.tasks.components.containers.NormalizedLandmark
+import org.checkerframework.checker.units.qual.Length
 import kotlin.math.atan2
 
 class OverlayView(context: Context?, attrs: AttributeSet?) :
@@ -44,6 +45,13 @@ class OverlayView(context: Context?, attrs: AttributeSet?) :
     private var imageHeight: Int = 1
     private var redLinePaint = Paint()
     private var range = 30
+    private var joint1 =0
+    private var joint2 =0
+    private var joint3 =0
+    private var expectedAngle=0.0
+    private var angle=0.0
+
+
 
 
     private var textPaint = Paint().apply {
@@ -72,24 +80,37 @@ class OverlayView(context: Context?, attrs: AttributeSet?) :
         FirebaseManager.fetchJointsAndAngles(sportName, techniqueName) { joints ->
             allJoints.clear()
             allJoints.addAll(joints.map { joint ->
-                mapOf(
-                    "joint1" to (joint["joint1"] as Long).toInt(),
-                    "joint2" to (joint["joint2"] as Long).toInt(),
-                    "joint3" to (joint["joint3"] as Long).toInt(),
-                    "expectedAngle" to joint["expectedAngle"] as Long // Convert Long to Double
+                val joint1 = (joint["joint1"] as Long).toInt()
+                val joint2 = (joint["joint2"] as Long).toInt()
+                val expectedAngle = joint["expectedAngle"] as Long
+
+                val jointMap = mutableMapOf<String, Any>(
+                    "joint1" to joint1,
+                    "joint2" to joint2,
+                    "expectedAngle" to expectedAngle
                 )
+
+                if (joint.containsKey("joint3")) {
+                    val joint3 = (joint["joint3"] as Long).toInt()
+                    jointMap["joint3"] = joint3
+                }
+
+                jointMap
             })
             invalidate() // Call this to redraw the view with the new data
             Log.d(TAG, "Updated joints data: $allJoints")
         }
     }
 
+
+
+
     private fun addNewTechnique() {
         val sportName = "Sprint"
         val techniqueName = "Technique1"
         val jointsData = listOf(
             mapOf("joint1" to 24, "joint2" to 26, "joint3" to 28, "expectedAngle" to 90),
-            mapOf("joint1" to 12, "joint2" to 14, "joint3" to 16, "expectedAngle" to 180),
+            mapOf("joint1" to 12, "joint2" to 14,  "expectedAngle" to 90),
             mapOf("joint1" to 23, "joint2" to 25, "joint3" to 27, "expectedAngle" to 120)
             // Add more joint sets as needed
         )
@@ -143,6 +164,7 @@ class OverlayView(context: Context?, attrs: AttributeSet?) :
 
             // Process landmarks and angles
             for (landmark in poseLandmarkerResult.landmarks()) {
+                Log.d(TAG, "brooo: $allJoints")
                 // Draw points
                 for (normalizedLandmark in landmark) {
                     canvas.drawPoint(
@@ -154,12 +176,26 @@ class OverlayView(context: Context?, attrs: AttributeSet?) :
 
                 // Calculate angles and determine line colors
                 for (angleSet in allJoints) {
-                    val joint1 = angleSet["joint1"] as Int
-                    val joint2 = angleSet["joint2"] as Int
-                    val joint3 = angleSet["joint3"] as Int
-                    val expectedAngle = (angleSet["expectedAngle"] as Long).toDouble() // Convert Long to Double
+                    val aSize= angleSet.size
+                    Log.d(TAG, "brooo: $aSize")
+                    if (angleSet.size ==4) {
+                        joint1 = angleSet["joint1"] as Int
+                        joint2 = angleSet["joint2"] as Int
+                        joint3 = angleSet["joint3"] as Int
+                        expectedAngle = (angleSet["expectedAngle"] as Long).toDouble()
+                        angle = getAngle(landmark, joint1, joint2, joint3)
+                    }
+                    else {
+                        joint1 = angleSet["joint1"] as Int
+                        joint2 = angleSet["joint2"] as Int
+                        expectedAngle = (angleSet["expectedAngle"] as Long).toDouble()
+                        angle = getAngle(landmark, joint1, joint2)
 
-                    val angle = getAngle(landmark, joint1, joint2, joint3)
+                    }
+
+
+
+
                     val useRedPaint = (angle < expectedAngle - range || angle > expectedAngle + range)
 
                     Log.d(TAG, "Angle: $angle")
@@ -171,11 +207,21 @@ class OverlayView(context: Context?, attrs: AttributeSet?) :
                         val endX = poseLandmarkerResult.landmarks()[0][poseLandmark.end()].x() * imageWidth * scaleFactor
                         val endY = poseLandmarkerResult.landmarks()[0][poseLandmark.end()].y() * imageHeight * scaleFactor
 
-                        if ((poseLandmark.start() == joint2 && poseLandmark.end() == joint3) ||
-                            (poseLandmark.start() == joint1 && poseLandmark.end() == joint2) ||
-                            (poseLandmark.start() == joint3 && poseLandmark.end() == joint1)) {
-                            linesToDraw.add(LineData(startX, startY, endX, endY, angle, expectedAngle, useRedPaint))
-                        } 
+                        if (angleSet.size ==4){
+
+                            if ((poseLandmark.start() == joint2 && poseLandmark.end() == joint3) ||
+                                (poseLandmark.start() == joint1 && poseLandmark.end() == joint2) ||
+                                (poseLandmark.start() == joint3 && poseLandmark.end() == joint1)) {
+                                linesToDraw.add(LineData(startX, startY, endX, endY, angle, expectedAngle, useRedPaint))
+                            }
+                        }
+                        else if (angleSet.size ==3){
+                            if ((poseLandmark.start() == joint2 && poseLandmark.end() == joint1) ||
+                                (poseLandmark.start() == joint1 && poseLandmark.end() == joint2) ) {
+                                linesToDraw.add(LineData(startX, startY, endX, endY, angle, expectedAngle, useRedPaint))
+                            }
+                        }
+
                     }
                 }
             }
@@ -224,11 +270,32 @@ class OverlayView(context: Context?, attrs: AttributeSet?) :
         }
     }
 
+    private fun getAngle(landmarks: MutableList<NormalizedLandmark>, index1: Int, index2: Int): Double {
+        if (landmarks.size > index1 && landmarks.size > index2 ) {
+            return calculateAngle(
+                landmarks[index1].x(), landmarks[index2].x(),
+                landmarks[index1].y(), landmarks[index2].y()
+            )
+        } else {
+            Log.e(PoseLandmarkerHelper.TAG, "Invalid landmark indices provided.")
+            return -1.0 // Return -1.0 to indicate an error
+        }
+    }
+
     // Function to calculate the angle
     private fun calculateAngle(x1: Float, x2: Float, x3: Float, y1: Float, y2: Float, y3: Float): Double {
         val angle = Math.toDegrees(
             (atan2(y3 - y2, x3 - x2) -
                     atan2(y1 - y2, x1 - x2)).toDouble()
+        )
+
+        val absAngle = kotlin.math.abs(angle) % 360
+        return if (absAngle > 180) 360 - absAngle else absAngle
+    }
+
+    private fun calculateAngle(x1: Float, x2: Float,  y1: Float, y2: Float): Double {
+        val angle = Math.toDegrees(
+            (atan2(y1 - y2, x1 - x2)).toDouble()
         )
 
         val absAngle = kotlin.math.abs(angle) % 360
