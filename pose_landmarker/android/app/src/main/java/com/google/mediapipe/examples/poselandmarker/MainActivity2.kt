cbe0dab7669dea57facbd3b9007141955d2e76ab
JulianSilva2001 <sackjuliansilva@gmail.com>
2024-06-26 06:50:05 +0530

    technique and sport activities updated

--- pose_landmarker/android/app/src/main/java/com/google/mediapipe/examples/poselandmarker/MainActivity2.kt
+++ pose_landmarker/android/app/src/main/java/com/google/mediapipe/examples/poselandmarker/MainActivity2.kt
@@ -6,8 +6,10 @@ import android.os.Bundle
 import android.util.Log
 import android.widget.Button
 import com.google.mediapipe.examples.poselandmarker.techniques.ActivitySprint
+import com.google.mediapipe.examples.poselandmarker.techniques.ActivityWorkout
 
 private var sport= "Sprint"
+private var activity= "no"
 
 class MainActivity2 : AppCompatActivity() {
 
@@ -15,21 +17,36 @@ class MainActivity2 : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main2)
 
-        val buttonBB = findViewById<Button>(R.id.button5)
-        buttonBB.setOnClickListener {
+        val buttonS = findViewById<Button>(R.id.button5)
+        buttonS.setOnClickListener {
             sport= "Sprint"
+            activity = "ActivitySprint"
+            callActivity()
+        }
+        val buttonW = findViewById<Button>(R.id.button7)
+        buttonW.setOnClickListener {
+            sport= "Workout"
+            activity = "ActivityWorkout"
 
             callActivity()
         }
     }
 
     private fun callActivity() {
+        val targetActivity = when (activity) {
+            "ActivitySprint" -> ActivitySprint::class.java
+            "ActivityWorkout" -> ActivityWorkout::class.java
+            else -> null
+        }
 
-
-
-        val intent = Intent(this, ActivitySprint::class.java).also {
-            it.putExtra("EXTRA_MESSAGE", sport)
-            startActivity(it)
+        targetActivity?.let {
+            val intent = Intent(this, it).apply {
+                putExtra("EXTRA_MESSAGE", sport)
+            }
+            startActivity(intent)
+        } ?: run {
+            // Handle the case where the activity is not found
+            Log.e("MainActivity2", "Activity not found")
         }
     }
 }
